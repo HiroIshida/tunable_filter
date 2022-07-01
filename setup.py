fb0e425326ca2f5cf5847e05aee663e7719be739
HiroIshida <spitfire.docomo@gmail.com>
2022-07-01 18:23:00 +0900

    0.0.4

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ install_requires = [
 
 setup(
     name='tunable_filter',
-    version='0.0.3',
+    version='0.0.4',
     description='Tunable image filter',
     author='Hirokazu Ishida',
     author_email='h-ishida@jsk.imi.i.u-tokyo.ac.jp',
